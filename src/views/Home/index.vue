03d5d2e7864f761b7e10b41babc377ab172d7d17
zs-zc <3011121187@qq.com>
2022-06-28 16:51:54 +0800

    完成登录推出

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,9 +1,22 @@
 <template>
-  <div>home</div>
+  <div>
+    home
+    <el-button @click.native.prevent="logout">esc</el-button>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {};
+  },
+  methods: {
+    async logout() {
+      await this.$store.dispatch("user/logout");
+      this.$router.push("/login");
+    },
+  },
+};
 </script>
 
 <style>
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,22 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true,
-  lintOnSave: false
-})
+// const { defineConfig } = require('@vue/cli-service')
+// module.exports = defineConfig({
+//   transpileDependencies: true,
+//   lintOnSave: false
+// })
+module.exports = {
+  assetsDir: "assets",
+  lintOnSave: false,
+  devServer: {
+    proxy: {
+      "/dev-api": {
+        target: "http://127.0.0.1:9528",
+        // target:process.env.VUE_APP_BASE_URL,//
+        pathRewrite: {
+          //重写路径
+          ws: true,
+          changeOrigin: true,
+        },
+      },
+    },
+  },
+};
